b034352d78d4519529bbf0f8fbe2c0237311a094
Samuel Newman <mozzius@protonmail.com>
2025-03-02 14:21:32 +0000

    make VITE_PORT optional

--- packages/appview/src/lib/env.ts
+++ packages/appview/src/lib/env.ts
@@ -10,7 +10,7 @@ export const env = cleanEnv(process.env, {
   }),
   HOST: host({ devDefault: '127.0.0.1' }),
   PORT: port({ devDefault: 3001 }),
-  VITE_PORT: port({ devDefault: 3000 }),
+  VITE_PORT: port({ default: 3000 }),
   DB_PATH: str({ devDefault: ':memory:' }),
   COOKIE_SECRET: str({ devDefault: '0'.repeat(32) }),
   SERVICE_DID: str({ default: undefined }),
